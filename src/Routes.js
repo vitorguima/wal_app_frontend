567ca3a09e1ff55ec4f25f4c7faaa8563447f936
Vitor <vitorguima@gmail.com>
2022-04-03 17:50:11 -0300

    chore: create pages files and add redux to dependencies

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import PostsFeed from './pages/PostsFeed';
+import Register from './pages/Register';
+import UserPosts from './pages/UserPosts';
+
 function Routes() {
   return (
     <Switch>
-      <Redirect exact from="/" component="" />
-      <Route exact path="/feed" component="" />
-      <Route exact path="/posts/:id" component="" />
-      <Route exact path="register" component="" />
+      <Redirect exact from="/" component={ Home } />
+      <Route exact path="/feed" component={ PostsFeed } />
+      <Route exact path="/posts/:id" component={ UserPosts } />
+      <Route exact path="register" component={ Register } />
+      <Route componente={ NotFound } />
     </Switch>
   );
 }
