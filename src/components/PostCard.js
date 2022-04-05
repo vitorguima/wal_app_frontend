5972c2d3baaaf458d49174ec4e86ee1dae90432d
Vitor <vitorguima@gmail.com>
2022-04-04 22:47:53 -0300

    feat: create wall page, posts feed component, get posts service and post card component

---
+++ src/components/PostCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function PostsCard(props) {
+  const {
+    title,
+    content,
+    authorNickname,
+    createdAt,
+    updatedAt,
+  } = props;
+
+  return (
+    <div>PostsCard
+      <p>title: {title}</p>
+      <p>content: {content}</p>
+      <p>author: {authorNickname}</p>
+      <p>created at: {createdAt}</p>
+      <p>updated at: {updatedAt}</p>
+    </div>
+  )
+}
