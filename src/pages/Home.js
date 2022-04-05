cc4b999da3ad4df0ea5881c5ecbaa4261d317de1
Vitor <vitorguima@gmail.com>
2022-04-04 21:03:12 -0300

    feat: implement register component and its service

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
 export default function Home() {
   return (
     <div>
       <p>Home</p>
+      <LoginForm />
     </div>
   )
 }
