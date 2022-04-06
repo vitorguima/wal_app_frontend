1f199cdbcb7f754e9f59acc2500ab8df7b9c3e57
vitorguima <vitorguima@gmail.com>
2022-04-06 19:50:48 -0300

    feat: change header render Logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 
 import { HeaderBiggerWrapper, HeaderMinorWrapper, HeaderWrapper } from '../styles/header';
 
 export default function Header() {
+  const { hasAuthentication, isFeedLoading } = useContext(AppContext);
+
+  const renderHeaderItems = () =>  {
+    if (hasAuthentication) {
+      return(
+        <>
+        <HeaderBiggerWrapper />
+          <HeaderMinorWrapper>
+            <div>
+              <span>Logout</span>
+            </div>
+        </HeaderMinorWrapper>
+        </>
+      )
+    } else {
+      return (
+        <HeaderBiggerWrapper>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <span>
+              Sign in
+            </span>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <span>
+              Sign up
+            </span>
+          </Link>
+        </HeaderBiggerWrapper>
+      ) 
+    }
+  }
+
   return (
     <HeaderWrapper>
-      <HeaderBiggerWrapper>
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          <span>
-            Home
-          </span>
-        </Link>
-        <Link to="/feed" style={{ textDecoration: 'none' }}>
-          <span>
-            Posts
-          </span>
-        </Link>
-      </HeaderBiggerWrapper>
-      <HeaderMinorWrapper>
-        <div>
-          <span>Logout</span>
-        </div>
-      </HeaderMinorWrapper>
+      { isFeedLoading ? <p>Loading</p> : renderHeaderItems() }
     </HeaderWrapper>
   )
 }
