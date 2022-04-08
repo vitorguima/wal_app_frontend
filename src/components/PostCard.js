36417a63a3cb2ddc1f43c25ffe9116b90fb81107
vitorguima <vitorguima@gmail.com>
2022-04-07 21:48:15 -0300

    refactor(style): change postcard style

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { PostCard, PostTitle, PostDetails, PostContent } from '../styles/PostCard';
 
+const convertData = (unformattedDate) => {
+  const date = new Date(unformattedDate);
+  const formattedData = date.toLocaleString();
+  return formattedData;
+}
+
 export default function PostsCard(props) {
   const {
     title,
@@ -12,11 +18,11 @@ export default function PostsCard(props) {
 
   return (
     <PostCard>
-      <PostTitle>title: {title}</PostTitle>
+      <PostTitle>{title}</PostTitle>
       <PostDetails>
         <p>Author: {authorNickname}</p>
-        <p>Created at: {createdAt}</p>
-        <p>Updated at: {updatedAt}</p>
+        <p>Created at: {convertData(createdAt)}</p>
+        <p>Updated at: {convertData(updatedAt)}</p>
       </PostDetails>
       <PostContent>
         <p>{content}</p>
--- src/styles/PostCard.js
+++ src/styles/PostCard.js
@@ -2,26 +2,28 @@ import styled from "styled-components";
 
 export const PostCard = styled.div`
   padding: 0 1rem 0 1rem;
-  border-bottom: black solid 2px;
-  margin: 2rem 8rem 0 8rem;
+  border-top: #0066b2 solid 2px;
+  margin: 1rem 8rem 1rem 8rem;
   width: 100vh;
   height: 25vh;
 `;
 
 export const PostTitle = styled.h1`
+  margin-top: 1rem;
 `;
 
 export const PostDetails = styled.p`
   display: flex;
   justify-content: space-between;
-  margin: 1rem 3rem 0rem 0rem;
-  font-size: 1.2rem;
+  margin: 0.5rem 3rem 0rem 0rem;
+  font-size: 0.9rem;
   width: 100%;
+  color: #707070;
 `;
 
 export const PostContent = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   width: 100%;
   word-wrap: break-word;
-  margin-top: 1rem;
+  margin-top: 2rem;
 `;
