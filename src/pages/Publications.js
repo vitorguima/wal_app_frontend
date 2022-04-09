afe6968aa7d03723312efbcb54587d47976d224f
Vitor <vitorguima@gmail.com>
2022-04-09 18:05:40 -0300

    feat: create getpostbyuser and insert currentpath variable to publications page

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import PostsFeed from '../components/PostsFeed'
 import Header from '../components/Header';
-import { FeedWrapper } from '../styles/PostsFeed'
+import { FeedWrapper } from '../styles/PostsFeed';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Publications() {
+  const currentPath = useLocation().pathname;
+
+  const feedMargin = {
+    '/publications': '7rem;',
+    '/feed': '0'
+  }
+
   return (
     <>
       <Header />
-      <FeedWrapper>
+      <FeedWrapper margin={feedMargin[currentPath]}>
         <PostsFeed />
       </FeedWrapper>
     </>
