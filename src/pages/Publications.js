1397b77108e25442c7e8b038208a62f1064b78a7
Vitor <vitorguima@gmail.com>
2022-04-10 11:56:46 -0300

    feat: add token validation and redirect logic to publications component

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PostsFeed from '../components/PostsFeed'
 import Header from '../components/Header';
 import { FeedWrapper } from '../styles/PostsFeed';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useLocation, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Publications() {
   const currentPath = useLocation().pathname;
+  const history = useHistory();
 
   const feedMargin = {
     '/publications': '7rem;',
     '/feed': '0'
   }
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+
+    if (!token) return history.push('/');
+  }, [])
+  
 
   return (
     <>
