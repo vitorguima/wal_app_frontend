aace0a9d810f91449f83cf5024ebd061ec99f508
Vitor <vitorguima@gmail.com>
2022-04-11 19:18:26 -0300

    feat: add authentication logic to home page, if the users session persists, it will redirect to feed page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 import {
   HomeWrapper, FormWrapper, LogoWrapper, Logo, SubLogo,
 } from '../styles/home';
+import validateTokenService from '../services/authentication/validateTokenService';
 
 export default function Home() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const retrievedToken = localStorage.getItem('token');
+    if (!retrievedToken) {
+      return;
+    }
+
+    const validateToken = async () => {
+      const response = await validateTokenService(retrievedToken);
+
+      if (response.status === 200) {
+        history.push('/feed');
+      }
+    };
+
+    validateToken();
+  }, []);
+
   return (
     <HomeWrapper>
       <LogoWrapper>
