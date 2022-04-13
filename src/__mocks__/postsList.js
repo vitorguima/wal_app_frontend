b468789e4302bc3236b1ec1a1855b61f7d5f909e
vitorguima <vitorguima@gmail.com>
2022-04-12 21:25:49 -0300

    test: implement tests for PostsFeed component

---
+++ src/__mocks__/postsList.js
@@ -0,0 +1,20 @@
+const postListMock = [
+  {
+    id: 1,
+    title: 'title',
+    content: 'content',
+    author_nickname: 'vitorguima',
+    created_at: '2022-03-31T02:55:35.189Z',
+    updated_at: '2022-03-31T02:55:35.189Z',
+  },
+  {
+    id: 2,
+    title: 'teste postman 2',
+    content: 'teste postman',
+    author_nickname: 'vitorguima',
+    created_at: '2022-03-31T02:55:35.189Z',
+    updated_at: '2022-03-31T02:55:35.189Z',
+  },
+];
+
+export default postListMock;
